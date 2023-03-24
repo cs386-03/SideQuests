151e985deaf99ee38b4ed5634552cbe2b1df5ac7
Max Poole <mkp274@nau.edu>
2023-03-24 11:05:24 -0700

    updated to have some testable functions

--- website/homepage/todo.js
+++ website/homepage/todo.js
@@ -1,4 +1,10 @@
 // dropdown
+pointTotal = 0;
+taskTotal = 0;
+document.getElementById("pointCount").innerText = pointTotal;
+document.getElementById("taskCount").innerText = taskTotal;
+
+
 
 function showDropdown() {
     document.getElementById("tasks").style.display = "block";
@@ -37,6 +43,7 @@ for (index=0; index<close.length; index++) {
     close[index].onclick = function() {
         var div = this.parentElement;
         div.style.display = "none";
+        console.log("the button was pressed out of the function");
     }
 }
 
@@ -58,12 +65,25 @@ function newListItem() {
 
     var dateInput = document.getElementById("dateSelect").value;
     var dateText = document.createTextNode(dateInput);
+
+    var prioritySelect = document.getElementsByName('priority_level');
+          
+        for(i = 0; i < prioritySelect.length; i++) {
+            if(prioritySelect[i].checked)
+            priorityText = document.createTextNode(prioritySelect[i].value);
+        }
     
     var fillerText = document.createTextNode("\nDue: ");
 
+    var fillerText2 = document.createTextNode("\nPriority Level: ");
+
     var br = document.createElement("br");
+    var br2 = document.createElement("br");
 
     li.appendChild(inputText);
+    li.appendChild(br2);
+    li.appendChild(fillerText2);
+    li.appendChild(priorityText);
     li.appendChild(br);
     li.appendChild(fillerText);
     li.appendChild(dateText);
@@ -88,6 +108,7 @@ function newListItem() {
         close[index].onclick = function() {
             var div = this.parentElement;
             div.style.display = "none";
+            console.log("the button was pressed inside of the function");
         }
     }
 
@@ -103,7 +124,35 @@ function newListItem() {
         complete[index].onclick = function() {
             var div = this.parentElement;
             div.style.display = "none";
+            var priority = document.getElementsByName('priority_level');
+            for(i = 0; i < priority.length; i++) {
+                if(priority[i].checked)
+                points = priority[i].value;
+            }
+
+            totalTasks();
+            totalPoints( priority );
         }
     }
+
+}
+
+function totalPoints( priority ) {
+    points = Number(points);
+    pointTotal = points + pointTotal;
+
+    document.getElementById("pointCount").innerText = pointTotal;
+    console.log("total points ran");
+    return pointTotal;
+}
+
+function totalTasks() {
+    taskTotal = taskTotal + 1;
+
+    document.getElementById("taskCount").innerText = taskTotal;
+
+    console.log("total tasks ran");
+
+    return taskTotal;
 
 }
