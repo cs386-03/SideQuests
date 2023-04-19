82587317a34a1bc07f44b042c4b8139e6b17c4f1
Max Poole <mkp274@nau.edu>
2023-04-18 19:43:15 -0700

    added the sorting methods in the to-do list

--- website/homepage/todo.js
+++ website/homepage/todo.js
@@ -124,7 +124,8 @@ function newListItem() {
         complete[index].onclick = function() {
             var newIndex = 0;
             var div = this.parentElement;
-            div.style.display = "none";
+            //div.style.display = "none";
+            div.remove();
             
             var notIsFound = true;
             while(notIsFound) {
@@ -143,8 +144,18 @@ function newListItem() {
 
 }
 
+
+
 function totalPoints( points ) {
     points = Number(points);
+     if(points == 3){
+        points = 1;
+     }
+
+     else if( points == 1){
+        points = 3;
+     }
+
     pointTotal = points + pointTotal;
 
     document.getElementById("pointCount").innerText = pointTotal;
@@ -157,5 +168,410 @@ function totalTasks() {
     document.getElementById("taskCount").innerText = taskTotal;
 
     return taskTotal;
+
+}
+
+
+
+function sortByDate(){
+    var tempList, list, index, switching, listItem, shouldSwitch
+    tempList = []
+    list = document.getElementsByTagName("li");
+
+    for (var index = 0, length = list.length; index < length; index++){
+        tempList.push(list[index].innerHTML)
+    }
+
+    console.info(tempList);
+    switching = true;
+
+    while(switching) {
+        switching = false;
+
+        for(i = 0; i < (tempList.length - 1); i++){
+            shouldSwitch = false
+
+            var notIsFound = true;
+            var newIndex = 0;
+
+            while(notIsFound) {
+                if(tempList[i][newIndex - 2]==":" && tempList[i][newIndex - 3]=="e") {
+
+                    year = tempList[i][newIndex] + tempList[i][newIndex + 1] + tempList[i][newIndex + 2] + tempList[i][newIndex + 3];
+                    month = tempList[i][newIndex + 5] + tempList[i][newIndex + 6];
+                    day = tempList[i][newIndex + 8] + tempList[i][newIndex + 9];
+
+                    notIsFound = false;
+
+                }
+
+                newIndex = newIndex + 1;
+            }
+
+            notIsFound = true;
+            newIndex = 0;
+
+            while(notIsFound) {
+                if(tempList[i + 1][newIndex - 2]==":" && tempList[i + 1][newIndex - 3]=="e") {
+
+                    nextYear = tempList[i + 1][newIndex] + tempList[i][newIndex + 1] + tempList[i + 1][newIndex + 2] + tempList[i + 1][newIndex + 3];
+                    nextMonth = tempList[i + 1][newIndex + 5] + tempList[i + 1][newIndex + 6];
+                    nextDay = tempList[i + 1][newIndex + 8]+tempList[i + 1][newIndex + 9];
+
+                    notIsFound = false;
+
+                }
+
+                newIndex = newIndex + 1;
+            }
+ 
+            if(Number(year) > Number(nextYear)){
+
+                shouldSwitch = true;
+                break;
+
+            }
+
+            else if( Number(month) > Number(nextMonth)){
+
+                shouldSwitch = true;
+                break;
+
+            }
+
+            else if( Number(day) > Number(nextDay)){
+
+                shouldSwitch = true;
+                break;
+
+            }            
+        }
+        
+        if ( shouldSwitch ){
+            var swapValue = tempList[i + 1];
+            tempList[i + 1] = tempList[i];
+            tempList[i] = swapValue;
+            switching = true;
+        }
+    }
+
+    removeIndex = 0;
+    while( removeIndex < document.getElementById("todo_list").getElementsByTagName("li").length ){
+        const i = document.querySelector("li:last-child");
+        i.remove();
+    }
+
+    
+    addIndex = 0;
+
+
+    while (addIndex < tempList.length){
+        console.log("in the while loop");
+        var li = document.createElement("li");
+
+        var fillerText = document.createTextNode("\nDue: ");
+
+        var fillerText2 = document.createTextNode("\nPriority Level: ");
+
+        var br = document.createElement("br");
+
+        var br2 = document.createElement("br");
+
+        var inputValue = "";
+        var priority = 0;
+        var date = "";
+
+        searchIndex = 0;
+        notIsFound = true;
+        while( notIsFound ){
+            if( tempList[addIndex][searchIndex] == "<" ){
+                for(index = 0; index < searchIndex; index++){
+                    inputValue = inputValue + tempList[addIndex][index];
+                }
+                notIsFound = false;
+            }
+            searchIndex = searchIndex + 1;
+        }
+
+        searchIndex = 0;
+        notIsFound = true;
+        while( notIsFound ){
+            if( tempList[addIndex][searchIndex] == ":" ){
+                priority = tempList[addIndex][searchIndex + 2];
+                notIsFound = false;
+            }
+            searchIndex = searchIndex + 1;
+        }
+
+        searchIndex = 0;
+        notIsFound = true;
+        while( notIsFound ){
+            if( tempList[addIndex][searchIndex] == ":" && tempList[addIndex][searchIndex - 1] == "e" ){
+                for(index = 0; index < 10; index++){
+                    date = date + tempList[addIndex][searchIndex + 2 + index];
+                }
+                notIsFound = false;
+            }
+            searchIndex = searchIndex + 1;
+
+        }
+        inputText = document.createTextNode(inputValue);
+        priorityText = document.createTextNode(priority);
+        dateText = document.createTextNode(date);
+
+        li.appendChild(inputText);
+        li.appendChild(br2);
+        li.appendChild(fillerText2);
+        li.appendChild(priorityText);
+        li.appendChild(br);
+        li.appendChild(fillerText);
+        li.appendChild(dateText);
+
+        document.getElementById("todo_list").appendChild(li);
+
+        var closeButton = document.createElement("SPAN");
+        var closeText = document.createTextNode("X");
+
+        closeButton.className = "delete";
+        closeButton.appendChild(closeText);
+
+        li.appendChild(closeButton);
+
+        for (index=0; index<close.length; index++) {
+            close[index].onclick = function() {
+                var div = this.parentElement;
+                //div.style.display = "none";
+                div.remove();
+            }
+        }
+
+        var completeButton = document.createElement("SPAN");
+        var completeText = document.createTextNode("\u2713");
+
+        completeButton.className = "complete";
+        completeButton.appendChild(completeText);
+
+        li.appendChild(completeButton);
+
+        for (index=0; index<complete.length; index++) {
+            complete[index].onclick = function() {
+                var newIndex = 0;
+                var div = this.parentElement;
+                //div.style.display = "none";
+                div.remove();
+                
+                var notIsFound = true;
+                while(notIsFound) {
+                    if(div.innerHTML[newIndex - 2]==":") {
+                        points = div.innerHTML[newIndex];
+                        notIsFound = false;
+
+                    }
+                    newIndex = newIndex + 1;
+                }
+
+                totalTasks();
+                totalPoints( points );
+            }
+        }
+
+        addIndex = addIndex + 1;
+    }
+
+    tempList = [];
+    
+}
+
+
+
+function sortByPriority(){
+    var tempList, list, index, switching, listItem, shouldSwitch
+    tempList = []
+    list = document.getElementsByTagName("li");
+
+    for (var index = 0, length = list.length; index < length; index++){
+        tempList.push(list[index].innerHTML);
+        
+    }
+    console.info(tempList);
+
+    switching = true;
+
+    while(switching) {
+        switching = false;
+
+        for(i = 0; i < (tempList.length - 1); i++){
+            shouldSwitch = false
+
+            var notIsFound = true;
+            var newIndex = 0;
+
+            while(notIsFound) {
+                if(tempList[i][newIndex - 2]==":") {
+
+                    priority = tempList[i][newIndex];
+                    notIsFound = false;
+
+                }
+
+                newIndex = newIndex + 1;
+            }
+
+            notIsFound = true;
+            newIndex = 0;
+
+            while(notIsFound) {
+                if(tempList[i + 1][newIndex - 2]==":") {
+
+                    nextPriority = tempList[i + 1][newIndex]
+                    notIsFound = false;
+
+                }
+
+                newIndex = newIndex + 1;
+            }
+ 
+            if(Number(priority) > Number(nextPriority)){
+
+                shouldSwitch = true;
+                break;
+         
+            }
+
+        }
+        
+        if ( shouldSwitch ){
+            var swapValue = tempList[i + 1];
+            tempList[i + 1] = tempList[i];
+            tempList[i] = swapValue;
+            switching = true;
+        }   
+    }   
+
+
+    removeIndex = 0;
+    while( removeIndex < document.getElementById("todo_list").getElementsByTagName("li").length ){
+        const i = document.querySelector("li:last-child");
+        i.remove();
+    }
+
+    
+    addIndex = 0;
+
+
+    while (addIndex < tempList.length){
+        console.log("in the while loop");
+        var li = document.createElement("li");
+
+        var fillerText = document.createTextNode("\nDue: ");
+
+        var fillerText2 = document.createTextNode("\nPriority Level: ");
+
+        var br = document.createElement("br");
+
+        var br2 = document.createElement("br");
+
+        var inputValue = "";
+        var priority = 0;
+        var date = "";
+
+        searchIndex = 0;
+        notIsFound = true;
+        while( notIsFound ){
+            if( tempList[addIndex][searchIndex] == "<" ){
+                for(index = 0; index < searchIndex; index++){
+                    inputValue = inputValue + tempList[addIndex][index];
+                }
+                notIsFound = false;
+            }
+            searchIndex = searchIndex + 1;
+        }
+
+        searchIndex = 0;
+        notIsFound = true;
+        while( notIsFound ){
+            if( tempList[addIndex][searchIndex] == ":" ){
+                priority = tempList[addIndex][searchIndex + 2];
+                notIsFound = false;
+            }
+            searchIndex = searchIndex + 1;
+        }
+
+        searchIndex = 0;
+        notIsFound = true;
+        while( notIsFound ){
+            if( tempList[addIndex][searchIndex] == ":" && tempList[addIndex][searchIndex - 1] == "e" ){
+                for(index = 0; index < 10; index++){
+                    date = date + tempList[addIndex][searchIndex + 2 + index];
+                }
+                notIsFound = false;
+            }
+            searchIndex = searchIndex + 1;
+
+        }
+        inputText = document.createTextNode(inputValue);
+        priorityText = document.createTextNode(priority);
+        dateText = document.createTextNode(date);
+
+        li.appendChild(inputText);
+        li.appendChild(br2);
+        li.appendChild(fillerText2);
+        li.appendChild(priorityText);
+        li.appendChild(br);
+        li.appendChild(fillerText);
+        li.appendChild(dateText);
+
+        document.getElementById("todo_list").appendChild(li);
+
+        var closeButton = document.createElement("SPAN");
+        var closeText = document.createTextNode("X");
+
+        closeButton.className = "delete";
+        closeButton.appendChild(closeText);
+
+        li.appendChild(closeButton);
+
+        for (index=0; index<close.length; index++) {
+            close[index].onclick = function() {
+                var div = this.parentElement;
+                //div.style.display = "none";
+                div.remove();
+            }
+        }
+
+        var completeButton = document.createElement("SPAN");
+        var completeText = document.createTextNode("\u2713");
+
+        completeButton.className = "complete";
+        completeButton.appendChild(completeText);
+
+        li.appendChild(completeButton);
+
+        for (index=0; index<complete.length; index++) {
+            complete[index].onclick = function() {
+                var newIndex = 0;
+                var div = this.parentElement;
+                //div.style.display = "none";
+                div.remove();
+                
+                var notIsFound = true;
+                while(notIsFound) {
+                    if(div.innerHTML[newIndex - 2]==":") {
+                        points = div.innerHTML[newIndex];
+                        notIsFound = false;
+
+                    }
+                    newIndex = newIndex + 1;
+                }
+
+                totalTasks();
+                totalPoints( points );
+            }
+        }
+
+        addIndex = addIndex + 1;
+    }
+
+    tempList = [];
 
 }
