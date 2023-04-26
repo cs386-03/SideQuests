40cc59cf81bff8493024ab5b5d5c0a551f8df8bd
mpoole1027 <100051927+mpoole1027@users.noreply.github.com>
2023-04-25 23:09:54 -0700

    added bubble sort to test

--- website/public/homepage/todo.js
+++ website/public/homepage/todo.js
@@ -571,3 +571,28 @@ function sortByPriority(){
     tempList = [];
 
 }
+
+
+function bubbleSort( array ) {
+    var outerIndex = 0;
+    var innerIndex = 0;
+    var temp;
+
+    for (outerIndex = 0; outerIndex < array.length; outerIndex++) {
+
+        for (innerIndex = 0; innerIndex < array.length; innerIndex++) {
+
+            if ( array[innerIndex] > array[innerIndex + 1] ) {
+
+                temp = array[innerIndex];
+
+                array[innerIndex] = array[innerIndex + 1];
+                
+                array[innerIndex + 1] = temp;
+
+            }
+        }
+    }
+
+    return array;
+}
