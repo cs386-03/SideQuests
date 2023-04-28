60a9686d9920adf83778dfe7a7525ea1b712364e
Lauren Farr <lkf57@nau.edu>
2023-04-28 09:03:24 -0700

    removed some old test files

--- Tests/daysInMonth.js
+++
@@ -1,9 +0,0 @@
-function daysInMonth(month, year)
-{
-    var d = new Date(year, month+1, 0);
-    console.log("Days In Month Calculated");
-    return d.getDate();
-
-}
-
-module.exports = daysInMonth;
--- Tests/totalPoints.js
+++
@@ -1,9 +0,0 @@
-function totalPoints( points, pointTotal ) {
-    points = Number(points);
-    pointTotal = points + pointTotal;
-
-    console.log("total points ran");
-    return pointTotal;
-}
-
-module.exports = totalPoints;
